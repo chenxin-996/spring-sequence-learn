441cd56a64b563703cf3011c652836c4ecafc61b
chenxin <email>
2023-07-18 21:42:03 +0800

    refactor: add some comments

--- 01spring-boot-starter/src/main/java/com/chenxin/springbootstarter/Application.java
+++ 01spring-boot-starter/src/main/java/com/chenxin/springbootstarter/Application.java
@@ -9,7 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+    // 官网文档地址：https://spring.io/projects/spring-boot
     public static void main(String[] args) {
+        // Spring Boot 的嵌入式 Apache Tomcat 服务器充当 Web 服务器，默认端口号 8080
         SpringApplication.run(Application.class, args);
     }
 
